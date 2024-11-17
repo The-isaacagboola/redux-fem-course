639965afa8bcb4d9598156f556a8c3d0b85b9a60
The-isaacagboola <isaacadeyemi9@gmail.com>
2024-11-17 21:30:17 +0100

    abstraction

--- src/setCounter.jsx
+++ src/setCounter.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { set } from "./actions";
 import useActions from "./useActions";
+import { useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
 
 export default function SetCounter() {
-  const [count, setCount] = useState(0);
+  const countFromStore = useSelector((state) => state.count);
+  const [count, setCount] = useState(+countFromStore);
 
+  // const actions = bindActionCreators({increment}, dispatch)
   //   const dispatch = useDispatch();
   // this works but we could also say;
 
   const action = useActions({ set });
 
+  useEffect(() => {
+    setCount(countFromStore);
+  }, [countFromStore]);
   return (
     <section className="set-count">
       <form>
@@ -17,8 +24,8 @@ export default function SetCounter() {
         <input
           id="set-to"
           type="number"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
+          value={count || ""}
+          onChange={(e) => setCount(+e.target.value)}
         />
         <input
           type="submit"
